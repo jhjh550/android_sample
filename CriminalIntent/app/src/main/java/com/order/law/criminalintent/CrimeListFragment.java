79658f55266e5ae3e15f6e9b645443e13ad6b2e9
jh park <jhjh550@gmail.com>
2015-01-14 20:25:11 +0900

    fragment_parameter : until p270

--- CriminalIntent/app/src/main/java/com/order/law/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/order/law/criminalintent/CrimeListFragment.java
@@ -1,8 +1,8 @@
 package com.order.law.criminalintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -59,6 +59,8 @@ public class CrimeListFragment extends ListFragment{
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         Crime c = (Crime) getListAdapter().getItem(position);
-        Log.d(TAG, c.getTitle()+" was clicked");
+        Intent i = new Intent(getActivity(), CrimeActivity.class);
+        i.putExtra(CrimeFragment.EXTRA_CRIME_ID, c.getId());
+        startActivity(i);
     }
 }
